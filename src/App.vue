46362523bc3203c6abd34c4c1129847488518da7
ledai94 <brian3dcnbo>
2024-12-27 15:24:20 +0700

    feat: add boostrap

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,38 @@
 
 <template>
   <header>header</header>
-
+  <button type="button" class="btn btn-primary">add boostrap</button>
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="../src/assets/img/img1.jpg" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="../src/assets/img/img1.jpg" class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="../src/assets/img/img1.jpg" class="d-block w-100" alt="..." />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
   <main>main</main>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+// import '@/assets/bootstrap-5.3.3/bootstrap-5.3.3/dist/css/bootstrap.min.css'
+// import '@/assets/bootstrap-5.3.3/bootstrap-5.3.3/dist/js/bootstrap.min.js'
+import '@/assets/dist/css/bootstrap.min.css'
+import '@/assets/dist/js/bootstrap.min.js';
 import './assets/base.css'
 
 import { createApp } from 'vue'
