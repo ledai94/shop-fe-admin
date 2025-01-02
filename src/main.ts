00defdd37db353311fc625540379bc7f5776a9d9
ledai94 <brian3dcnbo>
2025-01-02 15:42:11 +0700

    feat: add prettier and eslint

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 // import '@/assets/bootstrap-5.3.3/bootstrap-5.3.3/dist/css/bootstrap.min.css'
 // import '@/assets/bootstrap-5.3.3/bootstrap-5.3.3/dist/js/bootstrap.min.js'
-import '@/assets/dist/css/bootstrap.min.css'
-import '@/assets/dist/js/bootstrap.min.js';
-import './assets/base.css'
+import "@/assets/dist/css/bootstrap.min.css";
+import "@/assets/dist/js/bootstrap.min.js";
+import "./assets/base.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
